041fd1cc95d52bd96e71a78af6155b62d63f77a6
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-28 13:49:40 +0330

    add books app

--- bookStoreWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ bookStoreWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # third party
     "crispy_forms",
     "crispy_bootstrap4",
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     # local
     'accounts',
     'pages',
+    'books',
 
 ]
 
