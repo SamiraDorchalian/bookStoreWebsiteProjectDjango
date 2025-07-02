cc189365d1441928f3ed0898bc51a2c4dc03e242
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-02 14:30:25 +0330

    add translation to project

--- bookStoreWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
+++ bookStoreWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'books.context_processors.add_variable_to_context',
             ],
         },
     },
@@ -122,12 +123,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
+# LANGUAGE_CODE = 'En'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_I10N = True
 USE_TZ = True
 
 
