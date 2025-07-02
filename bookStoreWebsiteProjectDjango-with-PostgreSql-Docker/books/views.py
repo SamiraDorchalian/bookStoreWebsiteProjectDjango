c3088182a8d1abf149ceefb2206fe2df52ebbe65
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-02 11:17:15 +0330

    add login required to views

--- bookStoreWebsiteProjectDjango-with-PostgreSql-Docker/books/views.py
+++ bookStoreWebsiteProjectDjango-with-PostgreSql-Docker/books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import Book
 from .forms import CommentForm
@@ -15,7 +17,7 @@ class BookListView(generic.ListView):
 # class BookDetailView(generic.DetailView):
 #     model = Book
 #     template_name = 'books/book_detail.html'
-
+@login_required
 def book_detail_view(request, pk):
     # get book object
     book = get_object_or_404(Book, pk=pk)
@@ -40,13 +42,13 @@ def book_detail_view(request, pk):
     })
 
 
-class BookCreateView(generic.CreateView):
+class BookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_create.html'
 
 
-class BookUpdateView(generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_update.html'
