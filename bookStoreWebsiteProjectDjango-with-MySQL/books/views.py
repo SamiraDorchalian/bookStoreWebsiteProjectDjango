5132a1bf0d2a03415b534a3d68aaf90934cbb23f
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-03 12:18:41 +0330

    add BookCreateView

--- bookStoreWebsiteProjectDjango-with-MySQL/books/views.py
+++ bookStoreWebsiteProjectDjango-with-MySQL/books/views.py
@@ -3,12 +3,19 @@ from django.views import generic
 
 from .models import Book
 
+
 class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
+
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
-    
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'publisher', 'translator', 'description', 'price']
+    template_name = 'books/book_create.html'   
