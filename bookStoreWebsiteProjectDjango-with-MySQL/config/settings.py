565f12637cf60241ad2c1f3d30dd11ca665abd8c
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-03 16:35:53 +0330

    add media config to project

--- bookStoreWebsiteProjectDjango-with-MySQL/config/settings.py
+++ bookStoreWebsiteProjectDjango-with-MySQL/config/settings.py
@@ -150,3 +150,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
