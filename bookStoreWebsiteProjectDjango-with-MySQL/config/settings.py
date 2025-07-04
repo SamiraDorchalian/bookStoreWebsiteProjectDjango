3cb447c61792f5c68f07b90216169faf868b0507
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-04 12:13:28 +0330

    add translation to project

--- bookStoreWebsiteProjectDjango-with-MySQL/config/settings.py
+++ bookStoreWebsiteProjectDjango-with-MySQL/config/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'books.context_processors.add_variable_to_context',
             ],
         },
     },
@@ -119,12 +120,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_I10N = True
 USE_TZ = True
 
 
